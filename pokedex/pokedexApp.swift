a08c326dfe20d45642876ef7bb98029c15d47c7c
sfwgtlab <81831447+sfwgtlab@users.noreply.github.com>
2023-08-21 06:55:28 -0600

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Irving on 21/08/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
